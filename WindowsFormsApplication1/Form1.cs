f57f19f815ad7f7fc9de2bc2c5cb0cfdcc5b509c
Timo Koponen <kopontim@gmail.com>
2016-05-16 11:22:00 +0300

    Versio1 - ei toimiva

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// serializer_UnknownAttribute
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void serializer_UnknownAttribute
+        (object sender, XmlAttributeEventArgs e)
+        {
+            System.Xml.XmlAttribute attr = e.Attr;
+            Console.WriteLine("Unknown attribute " +
+            attr.Name + "='" + attr.Value + "'");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serializer_UnknownAttribute;
+        }
+    }
+
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [Serializable()]
+        public class Car
+        {
+            [System.Xml.Serialization.XmlElementAttribute("StockNumber")]
+            public string StockNumber { get; set; }
+
+            [System.Xml.Serialization.XmlElementAttribute("Make")]
+            public string Make { get; set; }
+
+            [System.Xml.Serialization.XmlElementAttribute("Model")]
+            public string Model { get; set; }
+        }
+.
+
+[System.Xml.Serialization.XmlRootAttribute("Cars", Namespace = "", IsNullable = false)]
+        public class Cars
+        {
+            [XmlArrayItem(typeof(Car))]
+            public Car[] Car { get; set; }
+
+        }
+.
+
+        public class CarSerializer
+        {
+            public Cars Deserialize()
+            {
+                Cars[] cars = null;
+                string path = HttpContext.Current.ApplicationInstance.Server.MapPath("~/App_Data/") + "cars.xml";
+
+                XmlSerializer serializer = new XmlSerializer(typeof(Cars[]));
+
+                StreamReader reader = new StreamReader(path);
+                reader.ReadToEnd();
+                cars = (Cars[])serializer.Deserialize(reader);
+                reader.Close();
+
+                return cars;
+            }
+        }
+    }
+}
