5afb98d4eec9bc2c41e4cb20b3f5e59912d1158c
Timo Koponen <kopontim@gmail.com>
2016-05-16 11:56:43 +0300

    liitetty väärä xml

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -44,15 +44,178 @@ namespace WindowsFormsApplication2
             [XmlArrayItem("Car", typeof(Car))]
             public Car[] Car { get; set; }
         }
-        The Deserialize function:
 
-        CarCollection cars = null;
-        string path = "cars.xml";
+        public class TeeSerialisointi
+        {
+            //The Deserialize function:
 
-        XmlSerializer serializer = new XmlSerializer(typeof(CarCollection));
+            CarCollection cars = null;
+            string path = "cars.xml";
 
-        StreamReader reader = new StreamReader(path);
-        cars = (CarCollection)serializer.Deserialize(reader);
-        reader.Close();
+            XmlSerializer serializer = new XmlSerializer(typeof(CarCollection));
+
+            StreamReader reader = new StreamReader(path);
+            cars = (CarCollection)serializer.Deserialize(reader);
+            reader.Close();
+        }
+
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+        public partial class doc
+        {
+
+            private docAssembly assemblyField;
+
+            private docMember[] membersField;
+
+            /// <remarks/>
+            public docAssembly assembly
+            {
+                get
+                {
+                    return this.assemblyField;
+                }
+                set
+                {
+                    this.assemblyField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlArrayItemAttribute("member", IsNullable = false)]
+            public docMember[] members
+            {
+                get
+                {
+                    return this.membersField;
+                }
+                set
+                {
+                    this.membersField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class docAssembly
+        {
+
+            private string nameField;
+
+            /// <remarks/>
+            public string name
+            {
+                get
+                {
+                    return this.nameField;
+                }
+                set
+                {
+                    this.nameField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class docMember
+        {
+
+            private docMemberSummary summaryField;
+
+            private string nameField;
+
+            /// <remarks/>
+            public docMemberSummary summary
+            {
+                get
+                {
+                    return this.summaryField;
+                }
+                set
+                {
+                    this.summaryField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlAttributeAttribute()]
+            public string name
+            {
+                get
+                {
+                    return this.nameField;
+                }
+                set
+                {
+                    this.nameField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class docMemberSummary
+        {
+
+            private docMemberSummarySee seeField;
+
+            private string[] textField;
+
+            /// <remarks/>
+            public docMemberSummarySee see
+            {
+                get
+                {
+                    return this.seeField;
+                }
+                set
+                {
+                    this.seeField = value;
+                }
+            }
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlTextAttribute()]
+            public string[] Text
+            {
+                get
+                {
+                    return this.textField;
+                }
+                set
+                {
+                    this.textField = value;
+                }
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+        public partial class docMemberSummarySee
+        {
+
+            private string crefField;
+
+            /// <remarks/>
+            [System.Xml.Serialization.XmlAttributeAttribute()]
+            public string cref
+            {
+                get
+                {
+                    return this.crefField;
+                }
+                set
+                {
+                    this.crefField = value;
+                }
+            }
+        }
+
+
+
     }
 }
