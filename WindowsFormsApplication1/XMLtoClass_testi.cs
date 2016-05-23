67956942457d808d378d8f4669ffc1d9015f198c
Timo Koponen <kopontim@gmail.com>
2016-05-23 10:38:54 +0300

    Lisätty XML to Class unitit

---
+++ WindowsFormsApplication1/XMLtoClass_testi.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+
+   /* 
+    Licensed under the Apache License, Version 2.0
+    
+    http://www.apache.org/licenses/LICENSE-2.0
+    */
+namespace Xml2CSharp
+{
+    [XmlRoot(ElementName = "content")]
+    public class Content
+    {
+        [XmlElement(ElementName = "textContent")]
+        public string TextContent {
+            get;
+            set;
+        }
+    }
+
+    [XmlRoot(ElementName = "contentInstance")]
+    public class ContentInstance
+    {
+        [XmlElement(ElementName = "content")]
+        public Content Content {
+            get;
+            set;
+        }
+        [XmlAttribute(AttributeName = "resourceName")]
+        public string ResourceName {
+            get;
+            set;
+        }
+        [XmlAttribute(AttributeName = "resourceUnit")]
+        public string ResourceUnit {
+            get;
+            set;
+        }
+        [XmlAttribute(AttributeName = "creationTime")]
+        public string CreationTime {
+            get;
+            set;
+        }
+    }
+
+    [XmlRoot(ElementName = "xml")]
+    public class Xml
+    {
+        [XmlElement(ElementName = "contentInstance")]
+        public List<ContentInstance> ContentInstance {
+            get;
+            set;
+        }
+    }
+
+}
+
---
+++ WindowsFormsApplication1/XMLtoClass_vaihtoehto.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Microsoft.VisualBasic;  
+//using System;  
+using System.Collections;  
+//using System.Collections.Generic;  
+using System.Data;  
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+// 25.5.2016 10:30
+
+public class xml
+{
+    string m_contentInstance = "";
+    public string contentInstance
+    {
+        get { return m_contentInstance; }
+        set { m_contentInstance = value; }
+    }
+
+    string m_contentInstance_resourceName = "";
+    public string contentInstance_resourceName
+    {
+        get { return m_contentInstance_resourceName; }
+        set { m_contentInstance_resourceName = value; }
+    }
+
+    string m_contentInstance_resourceUnit = "";
+    public string contentInstance_resourceUnit
+    {
+        get { return m_contentInstance_resourceUnit; }
+        set { m_contentInstance_resourceUnit = value; }
+    }
+
+    string m_contentInstance_creationTime = "";
+    public string contentInstance_creationTime
+    {
+        get { return m_contentInstance_creationTime; }
+        set { m_contentInstance_creationTime = value; }
+    }
+
+    string m_contentInstance = "";
+    public string contentInstance
+    {
+        get { return m_contentInstance; }
+        set { m_contentInstance = value; }
+    }
+
+    string m_contentInstance_resourceName = "";
+    public string contentInstance_resourceName
+    {
+        get { return m_contentInstance_resourceName; }
+        set { m_contentInstance_resourceName = value; }
+    }
+
+    string m_contentInstance_resourceUnit = "";
+    public string contentInstance_resourceUnit
+    {
+        get { return m_contentInstance_resourceUnit; }
+        set { m_contentInstance_resourceUnit = value; }
+    }
+
+    string m_contentInstance_creationTime = "";
+    public string contentInstance_creationTime
+    {
+        get { return m_contentInstance_creationTime; }
+        set { m_contentInstance_creationTime = value; }
+    }
+
+    string m_contentInstance = "";
+    public string contentInstance
+    {
+        get { return m_contentInstance; }
+        set { m_contentInstance = value; }
+    }
+
+    string m_contentInstance_resourceName = "";
+    public string contentInstance_resourceName
+    {
+        get { return m_contentInstance_resourceName; }
+        set { m_contentInstance_resourceName = value; }
+    }
+
+    string m_contentInstance_resourceUnit = "";
+    public string contentInstance_resourceUnit
+    {
+        get { return m_contentInstance_resourceUnit; }
+        set { m_contentInstance_resourceUnit = value; }
+    }
+
+    string m_contentInstance_creationTime = "";
+    public string contentInstance_creationTime
+    {
+        get { return m_contentInstance_creationTime; }
+        set { m_contentInstance_creationTime = value; }
+    }
+
+
+}  // class
+}  // namespace
