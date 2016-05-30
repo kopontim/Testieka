93f25ceb1780ccfaf034bb82661edae09c06ac9c
Timo Koponen <kopontim@gmail.com>
2016-05-30 10:30:23 +0300

    CarEsimerkki unit luotiin

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/CarEsimerkki.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace WindowsFormsApplication2
+{
+    class Class1
+    {
+    }
+
+    [Serializable()]
+    public class Car
+    {
+        [System.Xml.Serialization.XmlElement("StockNumber")]
+        public string StockNumber { get; set; }
+
+        [System.Xml.Serialization.XmlElement("Make")]
+        public string Make { get; set; }
+
+        [System.Xml.Serialization.XmlElement("Model")]
+        public string Model { get; set; }
+    }
+
+
+    [Serializable()]
+    [System.Xml.Serialization.XmlRoot("CarCollection")]
+    public class CarCollection
+    {
+        [XmlArray("Cars")]
+        [XmlArrayItem("Car", typeof(Car))]
+        public Car[] Car { get; set; }
+    }
+
+    public class TeeSerialisointi
+    {
+        //The Deserialize function:
+
+        CarCollection cars = null;
+        string path = "cars.xml";
+
+        XmlSerializer serializer = new XmlSerializer(typeof(CarCollection));
+
+        //            System.IO.StreamReader reader = new StreamReader(path);
+        StreamReader reader = new StreamReader("cars.xml");
+        CarCollection cars = (CarCollection)serializer.Deserialize(reader);
+        reader.Close();
+        }
+
+
+
+}
--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace WindowsFormsApplication2
 {
@@ -23,43 +20,6 @@ namespace WindowsFormsApplication2
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        }
-
-        [Serializable()]
-        public class Car
-        {
-            [System.Xml.Serialization.XmlElement("StockNumber")]
-            public string StockNumber { get; set; }
-
-            [System.Xml.Serialization.XmlElement("Make")]
-            public string Make { get; set; }
-
-            [System.Xml.Serialization.XmlElement("Model")]
-            public string Model { get; set; }
-        }
-
-
-        [Serializable()]
-        [System.Xml.Serialization.XmlRoot("CarCollection")]
-        public class CarCollection
-        {
-            [XmlArray("Cars")]
-            [XmlArrayItem("Car", typeof(Car))]
-            public Car[] Car { get; set; }
-        }
-
-        public class TeeSerialisointi
-        {
-            //The Deserialize function:
-
-            CarCollection cars = null;
-            string path = "cars.xml";
-
-            XmlSerializer serializer = new XmlSerializer(typeof(CarCollection));
-
-            System.IO.StreamReader reader = new StreamReader(path);
-        //    cars = (CarCollection)serializer.Deserialize(reader);
-        //    reader.Close();
         }
 
 
